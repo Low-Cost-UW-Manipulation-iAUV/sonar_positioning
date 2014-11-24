cc0bcd2a799b5ae5b126f2264c5097b451fe4080
Raphael Nagel <compiaffe@users.noreply.github.com>
2014-11-24 17:55:20 +0000

    added covariance search and determination if not found. Also it compiles.

--- include/sonar_positioning/sonar_position.hpp
+++ include/sonar_positioning/sonar_position.hpp
@@ -17,20 +17,30 @@ private:
 
     double sonar2Distance(const std_msgs::Int32MultiArray::ConstPtr&);
     double getOdomDistance(int, double, double);
-    void publish_position(void);
+    void publish_position(ros::Publisher);
 
     ros::NodeHandle nh_;
     ros::Subscriber sub_imu;
     ros::Subscriber sub_sonar;
-    ros::Publisher pub_position;
+    ros::Publisher pub_position_x;
+    ros::Publisher pub_position_y;
 
-    double yaw, pitch , roll;
+    double yaw, pitch, roll;
+    
     double x_position, y_position;
     double x_position_sum, y_position_sum;
     int x_position_counter, y_position_counter;
-    bool ready_x, ready_y;
+    bool variance_x_found, variance_y_found;
     ros::Time imu_timestamp;
-    unsigned int current_sonar_state;
+    int binsperaxis;
+
+    int calibration_length;
+    double variance_x, variance_y;
+    std::vector<double> x_variance_data;
+    std::vector<double> y_variance_data;
+
+    double mean(const std::vector<double>);
+    double std2(const std::vector<double>, const double mean);    
 };
 
 }
--- src/sonar_position.cpp
+++ src/sonar_position.cpp
@@ -13,23 +13,40 @@ sonar_position::sonar_position(ros::NodeHandle nh) {
     sub_imu = nh_.subscribe<nav_msgs::Odometry>("/odometry/filtered", 1, &sonar_position::sub_callback_imu, this);
     sub_sonar = nh_.subscribe<std_msgs::Int32MultiArray>("/sonar", 1, &sonar_position::sub_callback_sonar, this);
 
-    pub_position = nh_.advertise<nav_msgs::Odometry>("sonar/position", 100);
+    pub_position_x = nh_.advertise<nav_msgs::Odometry>("/sonar/position_x", 100);
+    pub_position_y = nh_.advertise<nav_msgs::Odometry>("/sonar/position_y", 100);
+
+    if (!nh_.getParam("/sonar/binsperaxis", binsperaxis)) {
+
+        ROS_ERROR("Sonar Position: Could not find binsperaxis, ending. \n");
+        ros::shutdown();
+    }
+
+    if (!nh_.getParam("/sonar/calibration_length", calibration_length)) {
+
+        ROS_ERROR("Sonar Position: Could not find calibration_length, assuming 1000. \n");
+        calibration_length = 1000;
+        nh_.setParam("/sonar/calibration_length", calibration_length);
+    }  
+    x_variance_data.clear();
+    x_variance_data.reserve(calibration_length);
+    y_variance_data.clear();
+    y_variance_data.reserve(calibration_length);
+
     yaw = 0;
     pitch = 0;
     roll = 0;
 
-    x_position = 0;
-    y_position = 0;
-
     x_position_sum = 0;
     y_position_sum = 0;
 
-    x_position_counter = 0;
-    y_position_counter = 0;
+    x_position_counter = 1;
+    y_position_counter = 1;
 
-    ready_x = false;
-    ready_y = false;
-    current_sonar_state = 0;
+    variance_x_found = false;
+    variance_y_found = false;
+    variance_x = 0;
+    variance_y = 0;
 
 
 }
@@ -55,61 +72,79 @@ void sonar_position::sub_callback_sonar(const std_msgs::Int32MultiArray::ConstPt
     // extract the important data from the sonar data
     int d_hypot = sonar2Distance(message);
 
-    // if we are looking ahead along x
+    // if we are looking ahead (along x)
     if(message->data[0] <= 45 && message->data[0] >= -45) {
         double odom_dist = getOdomDistance(d_hypot, (yaw + message->data[0]), pitch);
-        
-        // are we starting a new set of measurements along the x axis? --> this means the y axis was done and we should publish the data.
-        if(current_sonar_state != SURGE) {
-            current_sonar_state = SURGE;
+        x_position_counter ++;
+        x_position_sum += odom_dist;
+
+        // this should never happen
+        if(x_position_counter == 0) {
             x_position_counter = 1;
-            x_position_sum = odom_dist;
+            ROS_ERROR("sonar_position: the position counter x was somehow set to 0... This should NEVER happen.");
+        } 
+        // if we know the variance of the sonar positioning data
+        if(variance_x_found == true) {
+            // is the x axis measurement done? 
+            if(x_position_counter >= binsperaxis) {
+                // calculate the mean of the data.
+                x_position = x_position_sum / x_position_counter;  
 
-            // the y axis must be finished and we should publish its data.
-            if(ready_y == true) {
-                publish_position();
-            }
-        } else { // no, we are still working on the x axis.
-            x_position_counter ++;
-            x_position_sum += odom_dist;
-
-            // this should never happen
-            if(x_position_counter == 0) {
+                // publish it              
+                publish_position(pub_position_x);
                 x_position_counter = 1;
-                ROS_ERROR("sonar_position: the position counter x was somehow set to 0... This should NEVER happen.");
-            } 
-            x_position = x_position_sum / x_position_counter;
-            // we have stored a real measurement in the x_position 
-            ready_x = true;            
-        }
-    } else if (message->data[0] <= 135 && message->data[0] >45 ) {
-        double odom_dist = getOdomDistance(d_hypot, (yaw + message->data[0]), roll);
-        
-        // are we starting a new set of measurements along the x axis? --> this means the y axis was done and we should publish the data.
-        if(current_sonar_state != SWAY) {
-            current_sonar_state = SWAY;
-            y_position_counter = 1;
-            y_position_sum = odom_dist;
-
-            // the y axis must be finished and we should publish its data.
-            if(ready_x == true) {
-                publish_position();
+                x_position_sum = odom_dist;
             }
-        } else { // no, we are still working on the x axis.
-            y_position_counter ++;
-            y_position_sum += odom_dist;
+        } else { // we haven't got any variance data.
+            if(x_variance_data.size() < calibration_length) {
+                x_variance_data.push_back(x_position);
+                ROS_INFO("sonar_position: Determining Sonar variance on x - Sample %lu of %d",x_variance_data.size(), calibration_length);
+            } else {
+                variance_x = pow( std2(x_variance_data, mean(x_variance_data) ), 2);
+                ROS_INFO("sonar_position: Variance of x is %f, stored on param server", variance_x);
+                nh_.setParam("/sonar/variance/x", variance_x);
 
-            // this should never happen
-            if(y_position_counter == 0) {
+                variance_x_found = true;
+            }
+
+        } // We are looking at the y axis
+    } else if (message->data[0] <= 135 && message->data[0] >45 ) {
+        // find the adjascent (distance along y in 'odom' frame) if the d_hypot is the hypothenuse.
+            // We first look top down: undo the yaw and sonar angle. And then look at it from the side to counteract the current roll.
+        double odom_dist = getOdomDistance(d_hypot, (yaw + message->data[0]), roll);
+        y_position_counter ++;
+        y_position_sum += odom_dist;
+
+        // this should never happen
+        if(y_position_counter == 0) {
+            y_position_counter = 1;
+            ROS_ERROR("sonar_position: the position counter y was somehow set to 0... This should NEVER happen.");
+        } 
+        // if we know the variance of the sonar positioning data
+        if(variance_y_found == true) {
+            // is the x axis measurement done? 
+            if(y_position_counter >= binsperaxis) {
+                // calculate the mean of the data.
+                y_position = y_position_sum / y_position_counter;  
+
+                // publish it              
+                publish_position(pub_position_y);
                 y_position_counter = 1;
-                ROS_ERROR("sonar_position: the position counter y was somehow set to 0... This should NEVER happen.");
-            } 
-            y_position = y_position_sum / y_position_counter;
-            // we have stored a real measurement in the x_position 
-            ready_y = true;            
+                y_position_sum = odom_dist;
+            }
+        } else { // we haven't got any variance data.
+            if(y_variance_data.size() < calibration_length) {
+                y_variance_data.push_back(y_position);
+                ROS_INFO("sonar_position: Determining Sonar variance on y - Sample %lu of %d",y_variance_data.size(), calibration_length);                
+            } else {
+                variance_y = pow( std2(y_variance_data, mean(y_variance_data) ), 2);
+                ROS_INFO("sonar_position: Variance of y is %f, stored on param server", variance_y);
+                nh_.setParam("/sonar/variance/y", variance_y);
+
+                variance_y_found = true;
+            }
         }
     }
-
 
 }
 
@@ -147,11 +182,46 @@ double sonar_position::getOdomDistance(int body_position, double angle_a, double
 
 /** publish_position(): DOes what is says
 */
-void sonar_position::publish_position(void) {
+void sonar_position::publish_position(ros::Publisher pub) {
     nav_msgs::Odometry sonar_position;
+    sonar_position.header.stamp = ros::Time::now(); 
+    sonar_position.header.frame_id = "odom";
     sonar_position.pose.pose.position.x = x_position;
     sonar_position.pose.pose.position.y = y_position;
-    pub_position.publish(sonar_position);
+    sonar_position.pose.covariance.fill(0.0);
+    // set the variance data for x
+    sonar_position.pose.covariance[0] = variance_x;
+    // set the variance data for y
+    sonar_position.pose.covariance[7] = variance_y;
+
+    pub.publish(sonar_position);
+}
+
+
+/** mean(): calculates the mean
+*/
+double sonar_position::mean(const std::vector<double> vec) {
+    double sum = 0;
+    unsigned int size = 0;
+    for (typename std::vector<double>::const_iterator it = vec.begin(); it!=vec.end(); ++it, ++size) { 
+        sum += (*it);
+        // if size is not == 0, divide by size, else return 0
+    }
+    return (size)?(sum/size):0;
+}
+
+/** std2(): find the std_deviation across our current_range
+*/
+double sonar_position::std2(const std::vector<double> vec, const double mean) {
+    double sum = 0;
+    unsigned int size = 0;
+    for(typename std::vector<double>::const_iterator it = vec.begin(); it != vec.end(); ++it) {
+        sum+=std::pow(((*it) - mean),2);
+         ++size;
+    }
+    // if size is not == 0, divide by size, else return 0            
+    return size?std::sqrt(sum/size):-1;
+    
 }
 
 }  //end of namespace
@@ -173,6 +243,6 @@ int main(int argc, char **argv) {
     // register the 
     ros::spin();
 
-    ROS_INFO("imu_positioning: Shutting down ");
+    ROS_INFO("sonar_positioning: Shutting down ");
 
 }
